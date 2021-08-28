37ebb2142cd3fd46d1a333d955873321f005971e
Adam Birse <adambirse@gmail.com>
2021-08-28 21:59:35 +0100

    Tidied nav component

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -8,19 +8,15 @@ export const Navigation: React.FC = () => {
   return (
     <>
       <nav>
-        <p>
-          Navigation links currently causing signout, navigate with browser
-          instead.
-        </p>
         <ul>
           <li>
             <Link href="/">
-              <a>Home / </a>
+              <a>Home</a>
             </Link>
           </li>
           <li>
             <Link href="/protected">
-              <a>Protected resource /protected</a>
+              <a>Protected resource</a>
             </Link>
           </li>
         </ul>
