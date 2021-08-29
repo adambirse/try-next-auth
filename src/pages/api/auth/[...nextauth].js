0668da11ea3922711dfb7cf93dd3799284e43800
Adam Birse <adambirse@gmail.com>
2021-08-29 10:33:53 +0100

    sign in handler added.

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -9,4 +9,28 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  callbacks: {
+    /**
+     * @param  {object} user     User object
+     * @param  {object} account  Provider account
+     * @param  {object} profile  Provider profile
+     * @return {boolean|string}  Return `true` to allow sign in
+     *                           Return `false` to deny access
+     *                           Return `string` to redirect to (eg.: "/unauthorized")
+     */
+    async signIn(user, account, profile) {
+      console.log(`Called sign in handler with ${JSON.stringify(user)}`);
+
+      const isAllowedToSignIn = true;
+      return isAllowedToSignIn;
+      // if (isAllowedToSignIn) {
+      //   return true;
+      // } else {
+      //   // Return false to display a default error message
+      //   return false;
+      //   // Or you can return a URL to redirect to:
+      //   // return '/unauthorized'
+      // }
+    },
+  },
 });
